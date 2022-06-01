2de0ed7785239430e7ea512b522438d8a4c6dbf6
ashleyL25 <almcdowell.home@gmail.com>
2022-06-01 09:33:38 -0500

    offline

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -1,47 +1,47 @@
-
-const CACHE = "pwabuilder-offline";
-const QUEUE_NAME = "bgSyncQueue";
+// This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Cache name
+const CACHE = "pwabuilder-page";
 
-// Files required to make this app work offline
-var REQUIRED_FILES = [
-    'index.html'
-];
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
 
-self.addEventListener('install', function(event) {
-  // Perform install step:  loading each required file into cache
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Add all offline dependencies to the cache
-        return cache.addAll(REQUIRED_FILES);
-      })
-      .then(function() {
-        return self.skipWaiting();
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return the response from the cached version
-        if (response) {
-          return response;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-        // Not in cache - return the result from the live server
-        // `fetch` is essentially a "fallback"
-        return fetch(event.request);
-      }
-    )
-  );
-});
 
-self.addEventListener('activate', function(event) {
-  // Calling claim() to force a "controllerchange" event on navigator.serviceWorker
-  event.waitUntil(self.clients.claim());
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
